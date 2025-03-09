153350f7662c6407e97ae0d4d24aed513af73ab4
IPVA <ivanpereira345@gmail.com>
2025-03-09 12:45:19 -0300

    Final Modifier

--- src/axion/sakundoproject1/javacore/Fstaticmodifier/domain/Car.java
+++ src/axion/sakundoproject1/javacore/Fstaticmodifier/domain/Car.java
@@ -2,5 +2,11 @@ package axion.sakundoproject1.javacore.Fstaticmodifier.domain;
 
 public class Car {
     public static final double SPEED_LIMIT = 0;
+    public final Buyer BUYER = new Buyer();
+
+    public final void print() {
+        System.out.println("12345");
+    }
+
 
 }
---
+++ src/axion/sakundoproject1/javacore/Fstaticmodifier/domain/Ferrari.java
@@ -0,0 +1,10 @@
+package axion.sakundoproject1.javacore.Fstaticmodifier.domain;
+
+public class Ferrari extends Car{
+
+//    @Override
+//    public final void print() {
+//        System.out.println("46");
+//    }
+
+}
