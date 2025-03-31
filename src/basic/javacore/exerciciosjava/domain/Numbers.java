20f9912146e0ce95dac32fe3bf6e733cfacb7d80
IPVA <ivanpereira345@gmail.com>
2025-03-31 19:14:44 -0300

    Just initializing 2 variables with the same type

---
+++ src/basic/javacore/exerciciosjava/domain/Numbers.java
@@ -0,0 +1,12 @@
+package basic.javacore.exerciciosjava.domain;
+
+public class Numbers {
+
+    private final double[] coeffsA, coeffsB;
+
+
+    public Numbers(double[] coeffsA, double[] coeffsB) {
+        this.coeffsA = coeffsA;
+        this.coeffsB = coeffsB;
+    }
+}
